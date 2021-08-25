b85a484250970fd4dec4440c2005a1a973508fe8
kxdkxd <dhu_kxd@163.com>
2021-08-26 00:44:19 +0800

    Update raspberry_daemon.py

--- raspberry_onchip/raspberry_daemon.py
+++ raspberry_onchip/raspberry_daemon.py
@@ -13,29 +13,34 @@ LED_SLEEP_SPAN = 0.01
 emotions = []
 emo_led_map = {0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7}
 
+regex = re.compile(r"emo=(\d+)")
 
 def control_led_by_emotion(emo):
     gpio_control.set_all_leds('OFF')
-    time.sleep(LED_SLEEP_SPAN)
-    gpio_control.set_led(emo_led_map[emo], 'ON')
 
 
 if __name__ == '__main__':
-    gpio_control.reset_all_ports(*list(emo_led_map.values()))
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen(1)
-    client, address = server.accept()
-    while True:
-        data = client.recv(BUF_SIZE)
-        print(data.decode())
-        # client.close() 
-        regex = re.compile(r"emo=(\d+)")
-        emo_ids = re.findall(regex, data.decode())
-        if len(emo_ids) == 0:
-            continue
-        elif len(emo_ids) == 1: # Normal Situation
-            emo_id = int(emo_ids[0])
-            control_led_by_emotion(emo_id)
-        else: # Multiple Siuation
-            pass
+	gpio_control.reset_all_ports(*list(emo_led_map.values()))
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	server.bind((HOST, PORT))
+	while True:
+		server.listen(1)
+		client, address = server.accept()
+		while True:
+			data = client.recv(BUF_SIZE)
+			if len(data) == 0:
+				print("Client Closed....")
+				client.close()
+				break
+			print(data.decode())
+			emo_ids = re.findall(regex, data.decode())
+			if len(emo_ids) == 0:
+				continue
+			elif len(emo_ids) == 1: # Normal Situation
+				emo_id = int(emo_ids[0])
+				if emo_id not in emo_led_map.keys():
+					print("Wrong Emotion ID!")
+					continue
+				control_led_by_emotion(emo_id)
+			else: # Multiple Siuation
+				pass
